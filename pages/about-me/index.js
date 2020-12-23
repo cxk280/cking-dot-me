40bd14c7023eb3a00e321b3a283d201585faa11f
Christopher King <chris@cking.me>
2020-12-23 13:03:55 -0500

    style(about-me): Style About Me page

--- pages/about-me/index.js
+++ pages/about-me/index.js
@@ -1,15 +1,17 @@
 import markdownToHtml from '../../lib/markdownToHtml'
 import aboutMeMarkdown from '../../_markdown/aboutMe.md'
+import styles from '../../styles/Markdown.module.css'
 
 const Index = (props) => (
   <div
-    // className={markdownStyles['markdown']}
+    className={styles.markdown}
     dangerouslySetInnerHTML={{ __html: props.content }}
   />
 )
 
 Index.getInitialProps = async () => {
   const content = await markdownToHtml(aboutMeMarkdown)
+  console.log(`content: ${content}`)
   return { content: content }
 }
 
